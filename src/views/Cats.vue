4f7fc1fa274066d676ddac98339b3ad537c4ca24
bof <boris.graeff@gmail.com>
2019-03-18 10:38:02 +0100

    Display all cat breeds

--- src/views/Cats.vue
+++ src/views/Cats.vue
@@ -1,20 +1,47 @@
 <template>
   <div class="cats">
+    <ul>
+      <li v-for="breed in breeds" :key="breed.id">
+        <breed-card :breed="breed" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
+import BreedCard from './cats/BreedCard'
 
 export default {
   name: 'cats',
   mounted () {
     this.getBreeds()
   },
+  computed: {
+    ...mapState('breeds', {
+      breeds: ({ breeds }) => breeds
+    })
+  },
   methods: {
     ...mapActions('breeds', {
       getBreeds: 'getBreeds'
     })
+  },
+  components: {
+    BreedCard
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .cats {
+    ul {
+      display: flex;
+      flex-wrap: wrap;
+    }
+
+    li {
+      width: 25%;
+    }
+  }
+</style>
---
+++ src/views/cats/BreedCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>{{ breed.name }}</div>
+    <div>{{ breed. origin }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    breed: Object
+  }
+}
+</script>
